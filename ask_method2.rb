c34c7c81d2fe86c104d0063eae0d092efaa988c6
Alan Maciel <alan.maciel.salcedo@gmail.com>
2021-01-31 13:09:27 -0600

    improve ask uqestion method

--- ask_method2.rb
+++ ask_method2.rb
@@ -5,16 +5,10 @@ def ask question
     puts question
     reply = gets.chomp.downcase
 
-    if (reply == 'yes' || reply == 'no')
-      if reply == 'yes'
-        return true
-      end
-      if reply == 'no'
-        return false
-      end
-    else
-      puts 'Please answer "yes" or "no".'
-    end
+    return true if reply == 'yes'
+    return false if reply == 'no'
+
+    puts 'Please answer "yes" or "no".'
   end
 end
 
